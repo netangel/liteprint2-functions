d44d209f1ed4e8f5a92841bf6959e4d90dabe988
Artem Krivopolenov <akrivopolenov@gmail.com>
2021-11-17 22:10:13 +0100

    First step for auth function

--- AuthFunction/__init__.py
+++ AuthFunction/__init__.py
@@ -1,24 +1,60 @@
-import logging
-
 import azure.functions as func
+import logging
+import os
+import pymongo
 
+DEFAULT_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, access-control-allow-origin'
+}
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    if (req.method == "OPTIONS"):
+        return func.HttpResponse(None, headers=DEFAULT_HEADERS)
+    
+    appMode = os.environ['AZURE_FUNCTIONS_ENVIRONMENT'] 
+    email = None
+    password = None
+    
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        logging.error(f"Wrong request: {req.method}")
+    else:
+        email = req_body.get('email')
+        password = req_body.get('password')
+    
+    if appMode == 'Development':
+        url = os.environ['MONGO_DB_URL']
+    else:
+        url = None
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    dbClient = pymongo.MongoClient(url)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if email and password:
+        user = checkUserExists(email, password, dbClient)
+        if not user:
+            return func.HttpResponse(f"User with emaill {email} not found", status_code=400, headers=DEFAULT_HEADERS)
+        
+        session = createSession(user, dbClient)
+        
+        return func.HttpResponse({'sessionId': session}, headers=DEFAULT_HEADERS)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+            "Email or password is missing from the request",
+            status_code=400,
+            headers=DEFAULT_HEADERS
+        ) 
+
+def checkUserExists(email, password, dbClient) -> pymongo.ReturnDocument:
+    db = dbClient.db
+    return db.users.find_one({'email': email})
+
+def createSession(user, dbClient) -> str:
+    if user == None:
+        return None
+    else:
+        return "123abc"
+        
+    
