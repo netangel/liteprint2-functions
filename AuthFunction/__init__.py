bc3931246358e21bb9f936b1edb7858470c85f10
Artem Krivopolenov <akrivopolenov@gmail.com>
2021-11-20 23:40:05 +0100

    checking and adding users

--- AuthFunction/__init__.py
+++ AuthFunction/__init__.py
@@ -9,21 +9,22 @@ DEFAULT_HEADERS = {
     'Access-Control-Allow-Headers': 'Content-Type, access-control-allow-origin'
 }
 
+NEW_CLIENT_REQ = "newClient"
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     if (req.method == "OPTIONS"):
         return func.HttpResponse(None, headers=DEFAULT_HEADERS)
     
     appMode = os.environ['AZURE_FUNCTIONS_ENVIRONMENT'] 
-    email = None
-    password = None
+    email = password = reqType = None
     
     try:
         req_body = req.get_json()
-    except ValueError:
-        logging.error(f"Wrong request: {req.method}")
-    else:
         email = req_body.get('email')
         password = req_body.get('password')
+        reqType = req_body.get('requestType')
+    except ValueError:
+        logging.error(f"Wrong request: {req.method}")
     
     if appMode == 'Development':
         url = os.environ['MONGO_DB_URL']
@@ -32,29 +33,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     dbClient = pymongo.MongoClient(url)
 
-    if email and password:
-        user = checkUserExists(email, password, dbClient)
-        if not user:
-            return func.HttpResponse(f"User with emaill {email} not found", status_code=400, headers=DEFAULT_HEADERS)
-        
+    user = session = None
+    try:
+        user = checkOrCreateUser(email, password, reqType, dbClient)
         session = createSession(user, dbClient)
-        
-        return func.HttpResponse({'sessionId': session}, headers=DEFAULT_HEADERS)
+    except AttributeError as err:
+        return func.HttpResponse(str(err), status_code=400, headers=DEFAULT_HEADERS) 
     else:
-        return func.HttpResponse(
-            "Email or password is missing from the request",
-            status_code=400,
-            headers=DEFAULT_HEADERS
-        ) 
-
-def checkUserExists(email, password, dbClient) -> pymongo.ReturnDocument:
-    db = dbClient.db
-    return db.users.find_one({'email': email})
+        return func.HttpResponse(session, headers=DEFAULT_HEADERS)
+    
+def checkOrCreateUser(email, password, reqType, dbClient) -> pymongo.ReturnDocument:
+    if (email == "" or password == ""):
+        raise AttributeError("Email or password is missing from the request")
+    
+    clientData = {'email': email, 'password': password}
+    
+    usersDB = dbClient.db
+    user = usersDB.find_one(clientData)
+    
+    if (user and reqType == NEW_CLIENT_REQ):
+        raise AttributeError(f"Client with email {email} already exists!")
+    
+    if (reqType == NEW_CLIENT_REQ):
+        user = usersDB.insert_one(clientData)
+    
+    return user
+    
 
 def createSession(user, dbClient) -> str:
     if user == None:
-        return None
+        raise AttributeError("User not found!")
     else:
         return "123abc"
-        
-    
